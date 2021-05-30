e38efac1a84fb22ad12f58e45ed101ccc0a939ac
lanovia221 <84428327+lanovia221@users.noreply.github.com>
2021-05-30 23:03:27 +0700

    Menambah Splash Screen
    yaya

---
+++ src/main/java/com/example/snapwatch/Home.java
@@ -0,0 +1,4 @@
+package com.example.snapwatch;
+
+public class Home {
+}
---
+++ src/main/java/com/example/snapwatch/SplashActivity.java
@@ -0,0 +1,55 @@
+package com.example.snapwatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+    //variables
+    Animation topAnim, botAnim;
+    ImageView image;
+    TextView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // utk menghilangkan bar diatas
+        setContentView(R.layout.activity_splash);
+
+        //ANimation
+        topAnim = AnimationUtils.loadAnimation (this,R.anim.top_anim);
+        botAnim = AnimationUtils.loadAnimation (this,R.anim.buttom_anim);
+
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(botAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
+
+    }
+}
