26a9868ae7a7c31fa4185b9b82fc7ff7149998df
dhavara <83932613+dhavara@users.noreply.github.com>
2021-06-01 09:26:59 +0700

    Memo
    ini wes ada memo ne

--- src/main/java/com/example/snapwatch/MemoActivity.java
+++ src/main/java/com/example/snapwatch/MemoActivity.java
@@ -1,14 +1,103 @@
 package com.example.snapwatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MemoActivity extends AppCompatActivity {
+    private ListView listView;
+    static ArrayList<String> memo = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.memo_add, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.memo_add){
+            Intent intent = new Intent(getApplicationContext(), MemoEditActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo);
+        listView = findViewById(R.id.listView);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.snapwatch", Context.MODE_PRIVATE);
+
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("memo", null);
+
+        if (set == null){
+            memo.add("Example memo");
+        }else{
+            memo = new ArrayList(set);
+        }
+        
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, memo);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int id, long l) {
+                Intent intent = new Intent(getApplicationContext(), MemoEditActivity.class);
+                intent.putExtra("memoId", id);
+                startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long id) {
+                final int itemDeleted = i;
+
+                new AlertDialog.Builder(MemoActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Deleting Memo").setMessage("This action cannot be undone")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        memo.remove(itemDeleted);
+                        arrayAdapter.notifyDataSetChanged();
+
+                        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.snapwatch", Context.MODE_PRIVATE);
+                        HashSet<String> set = new HashSet<>(MemoActivity.memo);
+                        sharedPreferences.edit().putStringSet("memo", set).apply();
+                    }
+                })
+                        .setNegativeButton("No", null).show();
+
+                return true;
+            }
+        });
     }
 }
